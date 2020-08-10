a1d5c5b35ef596d32838c3ef99e2d8d08cd2a063
Holden Nakamura <holdenthehitcher@gmail.com>
2020-08-09 18:19:34 -1000

    added persistor

--- App.js
+++ App.js
@@ -5,16 +5,19 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import MainStackNavigator from "./src/navigation/MainStackNavigator";
 import { Provider } from "react-redux";
 import { ConfigureStore } from "./src/redux/ReduxStore";
+import { PersistGate } from "redux-persist/integration/react";
 
-const { store } = ConfigureStore();
+const { store, persistor } = ConfigureStore();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <MainStackNavigator />
-        <StatusBar />
-      </SafeAreaProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <SafeAreaProvider>
+          <MainStackNavigator />
+          <StatusBar />
+        </SafeAreaProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/ReduxStore.js
+++ src/redux/ReduxStore.js
@@ -1,16 +1,25 @@
-import { createStore, applyMiddleware, combineReducers } from "redux";
+import AsyncStorage from '@react-native-community/async-storage';
+import { createStore, combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
 import { ProfileActions } from "./ProfileStatsRedux";
 import { FoodsActions } from "./FoodsListRedux";
 import { MealsList } from "./MealsListRedux";
 
-export const ConfigureStore = () => {
-  const store = createStore(
-    combineReducers({
-      foodsActions: FoodsActions,
-      profileActions: ProfileActions,
-      mealsList: MealsList,
-    })
-  );
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage
+};
 
-  return { store };
+const rootReducer = combineReducers({
+  foodsActions: FoodsActions,
+  profileActions: ProfileActions,
+  mealsList: MealsList,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const ConfigureStore = () => {
+  const store = createStore(persistedReducer);
+  const persistor = persistStore(store);
+  return { store, persistor };
 };
